2c7a4c0bb68a820be9382eeed4f9605f3d9b0f70
shwetaanair <149757672+shwetaanair@users.noreply.github.com>
2024-04-05 10:32:42 +0530

    Create Implicitcursor.sql

---
+++ Implicitcursor.sql
@@ -0,0 +1,28 @@
+* Create table customer(cust_id varchar(5), name varchar(10),salary int);
+CUSTOMER TABLE CREATED!
+  
+* Insert into customer values('C01','Shwetha','23000');
+  Insert into customer values('C02','Sourabh','28000');
+  Insert into customer values('C03','Tejas','43000');
+  Insert into customer values('C04','Abhirami','53000');
+VALUES INSERTED!
+  
+select * from customer;
+TABLE WILL BE SHOWN!
+
+
+
+
+
+declare
+   total_rows number (3);
+begin
+   update CUSTOMER set salary = salary + 2000;
+   if sql%notfound then
+     dbms_output.put_line('no customer selected');
+   elsif sql%found then
+     total_rows:=sql%rowcount;
+     dbms_output.put_line(total_rows||'customer selected');
+     end if;
+     end;
+     /
